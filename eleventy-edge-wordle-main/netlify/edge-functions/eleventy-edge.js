93644b70a75f257917434705d197c5ee2b13bd04
maicadk <74359175+maicadk@users.noreply.github.com>
2022-05-09 16:54:53 -0700

    Add files via upload

---
+++ eleventy-edge-wordle-main/netlify/edge-functions/eleventy-edge.js
@@ -0,0 +1,37 @@
+import {EleventyEdge} from 'eleventy:edge';
+import precompiledAppData from './_generated/eleventy-edge-app-data.js';
+import formatGuessesAsTable, {formatUsedKeys} from '../../src/_11ty/table.js';
+import getTodaysSolution from './_solution/solution.js';
+
+export default async (request, context) => {
+	try {
+		let edge = new EleventyEdge('edge', {
+			request,
+			context,
+			precompiled: precompiledAppData,
+
+			// default is [], add more keys to opt-in e.g. ['appearance', 'username']
+			cookies: ['guesses', 'solution', 'currentDate', 'state'],
+		});
+
+		const solution = getTodaysSolution();
+		context.cookies.set({
+			name: 'solution',
+			value: solution,
+			path: '/',
+			httpOnly: true,
+			secure: true,
+			sameSite: 'Lax'
+		});
+
+		edge.config((eleventyConfig) => {
+			eleventyConfig.addFilter('table', formatGuessesAsTable);
+			eleventyConfig.addFilter('usedKeys', formatUsedKeys);
+		});
+
+		return await edge.handleResponse();
+	} catch (e) {
+		console.log('ERROR', {e});
+		return context.next(e);
+	}
+};
---
+++ eleventy-edge-wordle-main/netlify/edge-functions/guess.js
@@ -0,0 +1,77 @@
+//import {EdgeFunction} from 'netlify:edge';
+import getTodaysSolution from './_solution/solution.js';
+
+///** @type {EdgeFunction} */
+export default async (request, context) => {
+	let url = new URL(request.url);
+	const solution = getTodaysSolution();
+	
+	// Clean up old guesses
+	const [today] = new Date().toISOString().split('T');
+	if (context.cookies.get('currentDate') !== today) {
+		context.cookies.delete('guesses');
+		context.cookies.delete('state');
+
+		context.cookies.set({
+			name: 'currentDate',
+			value: today,
+			path: '/',
+			httpOnly: true,
+			secure: true,
+			sameSite: 'Lax'
+		});
+	}
+	
+	if (request.method === 'POST' && request.headers.get('content-type') === 'application/x-www-form-urlencoded') {
+		const body = await request.clone().formData();
+		const postData = Object.fromEntries(body);
+		
+		if (postData.guess) {
+			const guesses = (context.cookies.get('guesses') || '')
+				.split('|')
+				.filter(x => x.length > 0);
+
+			guesses.push(postData.guess.toLowerCase());
+			const newGuessCookie = guesses.join('|');
+
+			context.cookies.set({
+				name: 'guesses',
+				value: newGuessCookie,
+				path: '/',
+				httpOnly: true,
+				secure: true,
+				sameSite: 'Lax'
+			});
+
+			if (guesses.includes(solution)) {
+				context.cookies.set({
+					name: 'state',
+					value: 'win',
+					path: '/',
+					httpOnly: true,
+					secure: true,
+					sameSite: 'Lax'
+				});
+			} else if (guesses.length >= 6) {
+				context.cookies.set({
+					name: 'state',
+					value: 'loss',
+					path: '/',
+					httpOnly: true,
+					secure: true,
+					sameSite: 'Lax'
+				});
+			}
+		}
+
+		return new Response(
+			null,
+			{
+				status: 302,
+				headers: {
+					location: url.pathname
+				}
+			}
+		);
+	}
+};
